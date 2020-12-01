43106bb52d62865d89b070136e7c73a98f30d935
DayeonOh <dayeon.dev@gmail.com>
2020-12-01 20:59:51 +0900

    Initial commit

---
+++ src/SudokuFrame.java
@@ -0,0 +1,18 @@
+public class SudokuFrame {
+
+    private int[][] board = new int[9][9];
+
+
+    public SudokuFrame(){
+
+        //테스트용 board 생성기
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                board[i][j] = 9;
+            }
+        }
+
+        
+
+    }
+}

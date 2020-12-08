6b2683ac7ad7ddb6c8ca428685314d07b3570307
YangHyunSoo <hsyang501@gmail.com>
2020-12-08 20:26:55 +0900

    inti

--- src/SudokuPiece.java
+++ src/SudokuPiece.java
@@ -1,2 +1,18 @@
 public class SudokuPiece {
+    private int value;
+
+    public SudokuPiece(){
+
+    }
+    public SudokuPiece(int _value){
+        value = _value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }

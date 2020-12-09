5de89db5b966223e299840ed2b0b29966545152e
DayeonOh <dayeon.dev@gmail.com>
2020-12-09 22:31:22 +0900

    done

--- src/SudokuFrame.java
+++ src/SudokuFrame.java
@@ -31,33 +31,42 @@ public class SudokuFrame extends JFrame {
     }
 
     public SudokuFrame(SudokuBoard _board){
+
+
         board = _board;
-    	Container cp = getContentPane();
-    	cp.setLayout(new GridLayout(3, 1));
+    	JPanel cp = new JPanel();
+    	cp.setLayout(new BorderLayout());
         JPanel sudokuBoardPanel = new JPanel(new GridLayout(9, 9));
         button = new SudokuButton[9][9];
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 button[i][j] = new SudokuButton(board, this,1, i, j);
+                button[i][j].setPreferredSize(new Dimension(button_size, button_size));
 				sudokuBoardPanel.add(button[i][j]);
             }
         }
-        cp.add(sudokuBoardPanel);
-        cp.add(answer);
+        cp.add(sudokuBoardPanel, BorderLayout.NORTH);
+//        cp.add(answer);
 
         JPanel setNumberButtonPanel = new JPanel(new GridLayout(1, 9));
         setNumberButton = new SudokuSetNumberButton[9];
         for(int i = 0; i < 9; i++){
             setNumberButton[i] = new SudokuSetNumberButton(i+1, this, board);
             setNumberButton[i].setText("" + (i+1));
+            setNumberButton[i].setPreferredSize(new Dimension(button_size, button_size));
             setNumberButtonPanel.add(setNumberButton[i]);
         }
 
-        cp.add(setNumberButtonPanel);
+
+        cp.add(setNumberButtonPanel, BorderLayout.SOUTH);
 
         update();
+
+        JOptionPane.showMessageDialog(null, "[게임방법]\n화면은 9*9의 스도쿠 보드와 하단의 버튼으로 구성되어 있습니다.\n스도쿠 보드의 빈 칸을 클릭한 후 하단의 숫자 버튼으로 정답을 맞추어보세요.");
+
+        setContentPane(cp);
 		setTitle("Sudoku");
-		setSize(size * button_size + 10, size * button_size + 10);
+		setSize(size * button_size + 10, size * button_size + 110);
 		setVisible(true);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
